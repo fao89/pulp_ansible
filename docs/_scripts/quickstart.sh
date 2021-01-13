b49ea36522f098d381415f97229020797f27c61a
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-01-12 22:04:21 -0300

    Testing HTTPIE
    [noissue]

--- docs/_scripts/quickstart.sh
+++ docs/_scripts/quickstart.sh
@@ -9,7 +9,12 @@
 # variables)
 source setup.sh
 
+echo "Role - Workflows"
 source repo.sh
 source remote.sh
 source sync.sh
 
+echo "Collection - Workflows"
+source repo.sh bar
+source remote-collection.sh
+source sync.sh
