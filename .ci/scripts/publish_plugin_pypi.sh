59329aff26e117bb7a3447ef04b1fbc58b29c22a
Fabricio Aguiar <fabricio.aguiar@gmail.com>
2021-05-14 15:08:48 -0300

    Update CI + enable https
    [noissue]

--- .ci/scripts/publish_plugin_pypi.sh
+++ .ci/scripts/publish_plugin_pypi.sh
@@ -12,7 +12,9 @@ cd "$(dirname "$(realpath -e "$0")")"/../..
 
 set -euv
 
-export VERSION=$(http pulp/pulp/api/v3/status/ | jq --arg plugin ansible --arg legacy_plugin pulp_ansible -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
+export PULP_URL="${PULP_URL:-https://pulp}"
+
+export VERSION=$(http $PULP_URL/pulp/api/v3/status/ | jq --arg plugin ansible --arg legacy_plugin pulp_ansible -r '.versions[] | select(.component == $plugin or .component == $legacy_plugin) | .version')
 export response=$(curl --write-out %{http_code} --silent --output /dev/null https://pypi.org/project/pulp-ansible/$VERSION/)
 if [ "$response" == "200" ];
 then
